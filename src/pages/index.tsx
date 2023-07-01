7d4c382452eb985f11870ad7524e1f88ffbad37e
Adair Locks Grassi <adairlocksgrassi@gmail.com>
2023-07-01 18:06:24 -0300

    feat/adding model post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import { api } from "~/utils/api";
 import { SignUp, useUser, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -15,7 +15,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {user.isSignedIn && <SignOutButton />};{!user.isSignedIn && <SignUp />};
+        {user.isSignedIn && <SignOutButton />}
+        {!user.isSignedIn && <SignUp />}
+        <section>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </section>
       </main>
     </>
   );
